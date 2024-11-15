457f07a82f08c8a5f59d9fe3d7281b2686073ce3
MitranR <priyarajmitra@gmail.com>
2024-11-15 15:49:29 +0530

    Success

---
+++ mitran/mitran/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mitran project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mitran.settings')
+
+application = get_asgi_application()
